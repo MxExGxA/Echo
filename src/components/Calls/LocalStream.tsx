7393c359b46fe34c7d384c562fbfd6158774f514
MxExGxA <elsayedelghazy20@gmail.com>
2024-10-10 23:08:14 +0300

    returned audio tracks

--- src/components/Calls/LocalStream.tsx
+++ src/components/Calls/LocalStream.tsx
@@ -72,24 +72,24 @@ const LocalStream = ({
       Object.keys(peers).forEach((peer) => {
         if (stream) {
           //check if this peer has audio track on it
-          // const hasAudio = peers[peer]
-          //   .getSenders()
-          //   .find((sender) => sender.track?.kind === "audio");
-          // // if it has no  audio track, add our localstream audio track to it
-          // if (!hasAudio) {
-          //   try {
-          //     const audioTrack = stream?.getAudioTracks()[0];
-          //     if (audioTrack) {
-          //       peers[peer].addTransceiver(audioTrack, {
-          //         direction: "sendrecv",
-          //       });
-          //     }
-          //   } catch (err) {
-          //     throw new Error(
-          //       `Error: couldn't add audio track to peer ${peers[peer]}`
-          //     );
-          //   }
-          // }
+          const hasAudio = peers[peer]
+            .getSenders()
+            .find((sender) => sender.track?.kind === "audio");
+          // if it has no  audio track, add our localstream audio track to it
+          if (!hasAudio) {
+            try {
+              const audioTrack = stream?.getAudioTracks()[0];
+              if (audioTrack) {
+                peers[peer].addTransceiver(audioTrack, {
+                  direction: "sendrecv",
+                });
+              }
+            } catch (err) {
+              throw new Error(
+                `Error: couldn't add audio track to peer ${peers[peer]}`
+              );
+            }
+          }
           // // check if this peer has video track on it
           const videoTransceiver = peers[peer]
             .getTransceivers()
